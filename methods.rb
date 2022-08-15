2dda450ae88669955620c3e7a50531caddd25c0f
Fredrick <fredrick.lokidor@student.moringaschool.com>
2022-08-15 14:29:16 +0300

    commit tree 560f9ec21d8a5cb602e9f8988f5636717783b38b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_Programmer()
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name = "programmer")
+  puts (`Hello, #{name}!`);
+end
+
+def add(num1, num2) 
+   return num1 + num2;
+end
+
+def halve(number)
+        if number.class = String
+          return nil
+        end
+      
+        number / 2
+      end
